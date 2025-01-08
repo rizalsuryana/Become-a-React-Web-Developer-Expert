9262581751e1dd590a09ed67a585d6842e12ddf8
rizalsuryana <rizal.suryn@gmail.com>
2025-01-08 22:43:54 +0700

    bit update

--- src/components/ListMateri.jsx
+++ src/components/ListMateri.jsx
@@ -2,21 +2,21 @@ import { Link } from "react-router-dom";
 
 const ListMateri = () => {
     return(
-        <>
+        <div className="list-materi">
         <h2>Exercise List materi</h2>
         <ul>
-            <li>
+            <li className="item-materi">
                 <Link to='/counterapp'>Counter App</Link>
                 <p>Applying eslint to the generated code</p>
             </li>
-            <li>
+            <li className="item-materi">
                 <Link to='/stopwatchapp'>StopWatch Appp</Link>
                 <p>
                     Applying React StrichMode
                 </p>
             </li>
         </ul>
-        </>
+        </div>
     );
 
 }
