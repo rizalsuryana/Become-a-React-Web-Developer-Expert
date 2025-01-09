16b143fe5bf0cf21a331094f902b023df3e7c8b9
rizalsuryana <rizal.suryn@gmail.com>
2025-01-09 20:18:42 +0700

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage"
 import Footer from "./components/Footer"
 import CounterApp from "./components/counterapp/CounterApp"
 import StopwatchApp from "./components/stopwatch/StopwatchApp"
+import Store from "./components/store-state-tree//store"
 import { Routes, Route } from "react-router-dom"
 import './styles/style.css'
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/" element={<HomePage/>}/>
         <Route path="/counterapp" element={<CounterApp/>}/>
         <Route path="/stopwatchapp" element={<StopwatchApp/>}/>
+        <Route path="/store" element={<Store/>}/>
       </Routes>
     </main>
     <Footer/>
--- src/components/ListMateri.jsx
+++ src/components/ListMateri.jsx
@@ -15,6 +15,12 @@ const ListMateri = () => {
                     Applying React StrichMode
                 </p>
             </li>
+            <li className="item-materi">
+                <Link to='/store'>Store</Link>
+                <p>
+                    react redux
+                </p>
+            </li>
         </ul>
         </div>
     );
