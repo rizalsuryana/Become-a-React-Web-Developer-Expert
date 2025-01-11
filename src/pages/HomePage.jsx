e0bb74bb3d8706cbf9eb0620f99df4859c598f15
rizalsuryana <rizal.suryn@gmail.com>
2025-01-11 19:28:51 +0700

    ..

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,13 +2,13 @@ import ListMateri from "../components/ListMateri";
 const HomePage = () => {
 
     return(
-        <>
+        <div className="home-page">
         <h2>Become a React Developer Expert</h2>
         <p>
-          This is where I document every exercise I complete during my bootcamp. Stay tuned as I continue to add more! 🚀
+        This is where I document every exercise I complete during my bootcamp. Stay tuned as I continue to add more! 🚀
         </p>
         <ListMateri />
-      </>
+    </div>
     );
 }
 
