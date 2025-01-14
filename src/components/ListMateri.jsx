d697949605e462ff728495bf02a23adb828cacb6
Rizal Suryana <83992013+rizalsuryana@users.noreply.github.com>
2025-01-14 22:41:13 +0700

    na'am  habibi (#16)
    * ...
    
    * update.. :)
    
    * bit update
    
    * redux
    
    * dikit
    
    * ...
    
    * ..
    
    * update
    
    * fixing typo
    
    * ,
    
    * fixing bug
    
    * ..
    
    * rearrange
    
    * Branch before main (#8) (#9)
    
    * ...
    
    * update.. :)
    
    * bit update
    
    * redux
    
    * dikit
    
    * ...
    
    * ..
    
    * update
    
    * fixing typo
    
    * ,
    
    * fixing bug
    
    * ..
    
    * rearrange
    
    * update
    
    * update dikit
    
    * async action
    
    * applying to Goals components
    
    * renew code type rules
    
    * update list
    
    * Update code ... 👍🏼  (#14) (#15) add list
    
    * ...
    
    * update.. :)
    
    * bit update
    
    * redux
    
    * dikit
    
    * ...
    
    * ..
    
    * update
    
    * fixing typo
    
    * ,
    
    * fixing bug
    
    * ..
    
    * rearrange
    
    * Branch before main (#8) (#9)
    
    * ...
    
    * update.. :)
    
    * bit update
    
    * redux
    
    * dikit
    
    * ...
    
    * ..
    
    * update
    
    * fixing typo
    
    * ,
    
    * fixing bug
    
    * ..
    
    * rearrange
    
    * update
    
    * update dikit
    
    * async action
    
    * applying to Goals components
    
    * renew code type rules

--- src/components/ListMateri.jsx
+++ src/components/ListMateri.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom";
 
 const ListMateri = () => {
-return (
+  return (
     <div className="list-materi">
-    <h2>List of Exercises</h2>
-    <ul>
+      <h2>List of Exercises</h2>
+      <ul>
         <li className="item-materi">
-        <Link to="/counterapp">Counter App</Link>
-        <p>Applying ESLint to ensure code quality and catch potential errors</p>
+          <Link to="/counterapp">Counter App</Link>
+          <p>Applying ESLint to ensure code quality and catch potential errors</p>
         </li>
         <li className="item-materi">
-        <Link to="/stopwatchapp">Stopwatch App</Link>
-        <p>Using React Strict Mode for better error detection during development</p>
+          <Link to="/stopwatchapp">Stopwatch App</Link>
+          <p>Using React Strict Mode for better error detection during development</p>
         </li>
         <li className="item-materi">
-        <Link to="/redux">Redux</Link>
-        <p>Learning React Redux for state management</p>
+          <Link to="/redux">Redux</Link>
+          <p>Learning React Redux for state management</p>
         </li>
-    </ul>
+        <li className="item-materi">
+          <a href="https://rizal-open-space.vercel.app/" target="_blank" rel="noopener noreferrer">
+            Open Space: A Twitter-Like Social Media App
+          </a>
+          <p>Real World Project: Implementing Redux in a Social Media App (Twitter-Like)</p>
+        </li>
+      </ul>
     </div>
-);
+  );
 };
 
 export default ListMateri;
