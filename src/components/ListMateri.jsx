3e8576d929d2622a5803de579a9485be1ca1674f
Rizal Suryana <83992013+rizalsuryana@users.noreply.github.com>
2025-01-26 18:38:54 +0700

    Branch before main (#17)
    * ...
    
    * update.. :)
    
    * bit update
    
    * redux
    
    * dikit
    
    * ...
    
    * ..
    
    * update
    
    * fixing typo
    
    * ,
    
    * fixing bug
    
    * ..
    
    * rearrange
    
    * Branch before main (#8) (#9)
    
    * ...
    
    * update.. :)
    
    * bit update
    
    * redux
    
    * dikit
    
    * ...
    
    * ..
    
    * update
    
    * fixing typo
    
    * ,
    
    * fixing bug
    
    * ..
    
    * rearrange
    
    * update
    
    * update dikit
    
    * async action
    
    * applying to Goals components
    
    * renew code type rules
    
    * update list
    
    * Update code ... 👍🏼  (#14) (#15) add list
    
    * ...
    
    * update.. :)
    
    * bit update
    
    * redux
    
    * dikit
    
    * ...
    
    * ..
    
    * update
    
    * fixing typo
    
    * ,
    
    * fixing bug
    
    * ..
    
    * rearrange
    
    * Branch before main (#8) (#9)
    
    * ...
    
    * update.. :)
    
    * bit update
    
    * redux
    
    * dikit
    
    * ...
    
    * ..
    
    * update
    
    * fixing typo
    
    * ,
    
    * fixing bug
    
    * ..
    
    * rearrange
    
    * update
    
    * update dikit
    
    * async action
    
    * applying to Goals components
    
    * renew code type rules
    
    * add list

--- src/components/ListMateri.jsx
+++ src/components/ListMateri.jsx
@@ -23,8 +23,16 @@ const ListMateri = () => {
           </a>
           <p>Real World Project: Implementing Redux in a Social Media App (Twitter-Like)</p>
         </li>
+        <li className="item-materi">
+          <a href="https://rizal-discusser.vercel.app/" target="_blank" rel="noopener noreferrer">
+            Discusser: Discussion Forum App
+          </a>
+          <p>Project to Become a React-Web Developer Expert: <br/> 
+          Real case to Implementing Redux in a project application </p>
+        </li>
       </ul>
     </div>
+    
   );
 };
 
